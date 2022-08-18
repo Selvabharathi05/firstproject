adb4ef9bd6b24a09735ff8629c922bdd89da30be
Selvabharathi05 <bharathikavap@gmail.com>
2022-08-18 07:17:51 +0530

    firstpush

--- firstproject/Program.cs
+++ firstproject/Program.cs
@@ -19,7 +19,7 @@ namespace firstproject
             j = Convert.ToInt32(Console.ReadLine());
             k = i + j;
             Console.WriteLine("The value of k is" +" "+ k);
-            Console.WriteLine("first push");
+            Console.WriteLine("my first push");
             Console.ReadLine(); 
 
         }
