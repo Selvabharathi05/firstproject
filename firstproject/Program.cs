85abe75bd77c70d91d37959ecbb2bb20888577af
ADMIN <ADMIN@DESKTOP-DAUUJB2>
2022-08-18 06:43:25 +0530

    Add project files.

---
+++ firstproject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            int k;
+            Console.WriteLine("Enter value for i");
+            i =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for j");
+            j = Convert.ToInt32(Console.ReadLine());
+            k = i + j;
+            Console.WriteLine("The value of k is" +" "+ k);
+            Console.ReadLine(); 
+
+        }
+    }
+}
